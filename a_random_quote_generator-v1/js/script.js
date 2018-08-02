18143a5f7ba7951dbfc3b0af657ddbb81dae1228
Ernir Snær Helgason <ernir@Ernirs-MBP.lan>
2018-08-02 13:51:38 +0000

    Added an array of quotes

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -1,7 +1,42 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
+var quotes = [
+  {
+    quote    : "Spread love everywhere you go."
+    source   : "Mother Teresa"
+    citation : ""
+    year     : ""
+  },
 
+  {
+    quote    : "A champion is defined not by their wins but by how they can recover when they fall."
+    source   : "Serena Williams"
+    citation : ""
+    year     : ""
+  },
+  
+  {
+    quote    : "Don't judge each day by the harvest you reap but by the seeds that you plant."
+    source   : "Robert Louis Stevenson"
+    citation : ""
+    year     : "1871"
+  },
+
+  {
+    quote    : "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment."
+    source   : "Buddha"
+    citation : ""
+    year     : ""
+  },
+
+  {
+    quote    : "May the force be with you"
+    source   : "Han Solo"
+    citation : "Star Wars"
+    year     : "1977"
+  }
+];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
